c39eed9d53dc265b9babeb21fc9ac3f78d0c45db
lahlaliwassim2 <wlahlali34@gmail.com>
2022-11-27 02:02:35 +0100

    test

--- server/controllers/UsersController.js
+++ server/controllers/UsersController.js
@@ -29,6 +29,7 @@ module.exports.register = async(req,res,next) =>{
 module.exports.login = async(req,res,next) =>{
 
     try {
+        console.log('as')
         const {username,password} = req.body
         const user = await User.findById({ username })
         if(!user){
